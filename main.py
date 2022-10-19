ca26a45856cd24f642abf873dd4c63c39825d158
Сергей Полуэктов <seregatipich@Sergejs-MacBook-Air.local>
2022-10-19 08:10:45 +0200

    help

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import sqlite3
+import time
+
+import telebot
+from dotenv import load_dotenv
+
+from parse import get_info
+
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv('TOKEN_TELEGRAM'))
+
+while True:
+    bot.send_message('@kypitkvsrtirykiev', get_info)
+    time.sleep(60)
+
+
+if __name__ == '__main__':
+     bot.infinity_polling()
--- parse.py
+++ parse.py
@@ -1,8 +1,9 @@
-import sqlite3
 import os
 
 import requests
 from dotenv import load_dotenv
+
+from walls import wall_types
 
 load_dotenv()
 
@@ -10,22 +11,22 @@ load_dotenv()
 api_token = os.getenv('TOKEN_RIA')
 
 
-id_url = 'https://developers.ria.com/dom/info/22476055?api_key='
+id_url = f'https://developers.ria.com/dom/search?api_key={api_token}&category=1&realty_type=2&operation_type=1&state_id=10&city_id=10'
 
 
-def get_info(id_url):
-    response = requests.get(f'{id_url}{api_token}')
+def get_info(id_url, wall_types):
+    response = requests.get(f'{id_url}')
     properties = response.json().get('characteristics_values')
-    print(type(properties))
-    info = f"""Цена: {properties['234']}\n
-            Метро: {response.get('metro_station_name')}\n
+    wall_type_id = str(properties['118'])
+    info = f"""
+            Цена: {properties['234']}\n
+            Метро: {response.json().get('metro_station_name')}\n
             Этаж: {properties['227']}\n
             Этажность: {properties['228']}\n
             Общая площадь: {properties['214']}\n
             Жилая площадь: {properties['216']}\n
             Площадь кухни: {properties['218']}\n
-            Тип стен"""
-    print(info)
-
-
-get_info(id_url)
+            Тип стен: {wall_types[wall_type_id]['name']}\n
+            Количество комнат: {properties['209']}
+            """
+    return info
